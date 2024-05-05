e999e9cf4683ba3175c69f8623eb3cb59de5c178
Jessica Adamek <jessica.g.adamek@gmail.com>
2024-05-04 22:58:18 -0300

    :hammer: fix error

--- src/contexts/CycleContext.tsx
+++ src/contexts/CycleContext.tsx
@@ -9,6 +9,7 @@ import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
 import {
   addNewCycleAction,
   interruptCurrentCycleAction,
+  markCurrentCycleAsFinishedAction,
 } from '../reducers/cycles/actions'
 import { differenceInSeconds } from 'date-fns'
 interface CreateCycleData {
@@ -69,7 +70,7 @@ export function CyclesContextProvider({ children }: CycleContexProviderProps) {
   }
 
   function markCurrentCycleAsFinished() {
-    dispatch(markCurrentCycleAsFinished())
+    dispatch(markCurrentCycleAsFinishedAction())
   }
 
   function createNewCycle(data: CreateCycleData) {
